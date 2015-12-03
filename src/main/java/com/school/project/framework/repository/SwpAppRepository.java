51f1dd807b26550cc4932fa9a7cff1ef4f35b7ae
danny.wang <15251323382@163.com>
2015-12-03 11:21:57 +0800

    delete project5

--- src/main/java/com/school/project/framework/repository/SwpAppRepository.java
+++
@@ -1,9 +0,0 @@
-package com.school.project.framework.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.school.project.framework.models.App;
-
-public interface SwpAppRepository extends CrudRepository<App, String> {
-	
-}
--- src/main/java/com/school/project/framework/wxapi/exceptions/WxException.java
+++
@@ -1,6 +0,0 @@
-package com.school.project.framework.wxapi.exceptions;
-
-@SuppressWarnings("serial")
-public class WxException extends Exception {
-
-}
